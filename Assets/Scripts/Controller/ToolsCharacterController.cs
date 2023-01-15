4359ec73e1d1a7f2c97abfbb475607b99dcc1372
Paweł Adamczuk <pawel2000.adamczuk@student.uj.edu.pl>
2023-01-15 13:42:01 +0100

    adds tile map marker

--- Assets/Scripts/Controller/ToolsCharacterController.cs
+++ Assets/Scripts/Controller/ToolsCharacterController.cs
@@ -1,37 +1,46 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ToolsCharacterController : MonoBehaviour
 {
-    private CharacterController character;
-    private Rigidbody2D rgbd2d;
-    [SerializeField] private float offsetDistance = 1f;
-    [SerializeField] private float sizeOfInteractableArea = 1.2f;
+    CharacterController character;
+    Rigidbody2D rgbd2d;
+    [SerializeField] float offsetDistance = 1f;
+    [SerializeField] float sizeOfInteractableArea = 1.2f;
+    [SerializeField] MarkerManager markerManager;
+    [SerializeField] TileMapReadController tileMapReadcontroller;
 
     private void Awake()
     {
         character = GetComponent<CharacterController>();
         rgbd2d = GetComponent<Rigidbody2D>();
-        
     }
 
     private void Update()
     {
+        Marker();
         if (Input.GetMouseButtonDown(0))
         {
             UseTool();
         }
     }
 
-    void UseTool()
+    private void Marker()
+    {
+        Vector3Int gridPosition = tileMapReadcontroller.GetGridPosition(Input.mousePosition, true);
+        markerManager.markedCellPosition = gridPosition;
+    }
+
+    private void UseTool()
     {
         Vector2 position = rgbd2d.position + character.lastMotionVector * offsetDistance;
-        Collider2D[] collider = Physics2D.OverlapCircleAll(position, sizeOfInteractableArea);
 
-        foreach (var c in collider)
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(position, sizeOfInteractableArea);
+
+        foreach (Collider2D c in colliders)
         {
             ToolHit hit = c.GetComponent<ToolHit>();
             if (hit != null)
